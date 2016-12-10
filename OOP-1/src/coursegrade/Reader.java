accfed1c7a29c38faca9c036e49916da827c0628
amans <amans@AmansComputer>
2016-12-10 19:02:48 +0600

    newfile

---
+++ OOP-1/src/coursegrade/Reader.java
@@ -0,0 +1,15 @@
+package coursegrade;
+
+import java.util.*;
+
+public class Reader {
+	int i;
+	int k;
+	public void Scan(){
+		System.out.println("Please write number");
+		Scanner scn=new Scanner(System.in);
+		i = scn.nextInt();
+		System.out.println("Please write number");
+		k = scn.nextInt();
+	}
+}
